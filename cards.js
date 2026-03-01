8af7515ad9c8fdd001c645993eed7597ff865068
Lu-divine <lud1vine@protonmail.com>
2026-03-01 16:15:55 +0100

    Update cards.js
    Description de la vigie

--- cards.js
+++ cards.js
@@ -15,4 +15,12 @@ const oracleData = [
     "advice": "Nope, it is not in policy",
     "image": "./images/card2.png"
   }
+  {
+    "id": 3,
+    "name": "La vigie",
+    "category": "Gardienne",
+    "description": "La vigie est là, immobile en apparence mais les yeux grands ouverts, à scruter l’horizon numérique comme d’autres scrutent la mer en pleine tempête. Elle protège les âmes égarées qui s’aventurent un peu trop loin, celles qui cliquent sans lire, qui ouvrent sans douter ou qui installent sans vérifier. La vigie ne panique pas. Elle observe, elle corrèle, elle analyse. Elle lève un sourcil numérique au moindre comportement suspect. Elle ne sauvera peut-être pas le monde aujourd’hui, mais elle évitera que ton domaine finisse en colonie de vacances pour acteurs malveillants organisés.",
+    "advice": "Personne n’est invincible. La détection au plus tôt permet de minimiser les risques.",
+    "image": "./images/card3.png"
+  }
 ];
