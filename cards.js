724d65728d4669400b6d8cdfa54feb83b12d6ef2
kwouffe <kwouffe@users.noreply.github.com>
2026-03-01 15:17:10 +0100

    Add files via upload

---
+++ cards.js
@@ -0,0 +1,18 @@
+const oracleData = [
+  {
+    "id": 1,
+    "name": "Le patcheur",
+    "category": "Sagesse",
+    "description": "Le Patcheur est le maître patient et acharné de son parc informatique. Il ordonne ses mises à jour comme un empereur ordonne le monde, en plus petit, car il est humble et raisonnable. Cela évite bien des incidents et rapporte de la sérénité. Voilà comment on dort tranquille.",
+    "advice": "Patch first, think later.",
+    "image": "./images/card1.png"
+  },
+  {
+    "id": 2,
+    "name": "Le policy maker",
+    "category": "Patience",
+    "description": "Where is my policy.",
+    "advice": "NOPE",
+    "image": "./images/card2.png"
+  }
+];
